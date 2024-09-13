dd48f2c0d017d90748705ec9cc982a4c67b1c8e8
Moon <101085046+MOON-CODES@users.noreply.github.com>
2024-09-13 13:02:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,8 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/CyberPixelPro/AviaxMusic",
-)
+    "https://github.com/MOON-CODES/AviaxMusic"
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
