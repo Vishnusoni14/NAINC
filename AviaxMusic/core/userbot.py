d54fe5b40b400f12d3d6d04e033798b958bedce9
kakshi0962z <101088760+kakshi0962z@users.noreply.github.com>
2024-10-09 17:07:39 +0530

    Update userbot.py

--- AviaxMusic/core/userbot.py
+++ AviaxMusic/core/userbot.py
@@ -51,8 +51,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("AviaxOfficial")
-                await self.one.join_chat("AviaxSupport")
+                await self.one.join_chat("moon_sunlight")
+                await self.one.join_chat("moon_sunlight")
             except:
                 pass
             assistants.append(1)
